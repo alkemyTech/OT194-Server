63e72cea6fd34e600a5dc0bf6f9e0c84e62c5510
urreita9 <francisco.urrea9@gmail.com>
2022-05-22 20:30:45 -0300

    Started working. Hashed parrwod

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+	res.send('respond with a resource');
+});
+
+router.post('/auth/register', async (req, res) => {
+	const { firstName, lastName, email, password } = req.body;
+
+	const salt = bcryptjs.genSaltSync();
+
+	const hashedPassword = bcryptjs.hashSync(password, salt);
 });
 
 module.exports = router;
