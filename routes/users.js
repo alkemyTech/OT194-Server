5a9a6097eef4e492ce23db30e420e2571fa8ebfb
urreita9 <francisco.urrea9@gmail.com>
2022-05-22 21:55:58 -0300

    Express validator Ok. Password encryption ok. User saved in db ok.

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,26 @@
 const express = require('express');
-const bcryptjs = require('bcryptjs');
+const { register } = require('../controllers/user');
 const router = express.Router();
+const { check } = require('express-validator');
+const { validateFields } = require('../middleware/validateFields');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 	res.send('respond with a resource');
 });
 
-router.post('/auth/register', async (req, res) => {
-	const { firstName, lastName, email, password } = req.body;
-
-	const salt = bcryptjs.genSaltSync();
-
-	const hashedPassword = bcryptjs.hashSync(password, salt);
-});
+router.post(
+	'/auth/register',
+	[
+		check('email', 'Email is invalid').isEmail(),
+		check('password', 'password must be at least 6 characters long').isLength({
+			min: 6,
+		}),
+		check('firstName', 'firstName is required').not().isEmpty(),
+		check('lastName', 'lastName is required').not().isEmpty(),
+		validateFields,
+	],
+	register
+);
 
 module.exports = router;
