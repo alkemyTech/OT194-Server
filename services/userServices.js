baae05e468e23b29e1e44c5fa42fc5fe12c41cdf
Martin Farres <91481417+Tats01@users.noreply.github.com>
2022-05-22 23:45:25 -0300

    Nodemailer Service

---
+++ services/userServices.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+module.exports = {
+  async emailService(username, email) {
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: 'somosfundacionmas.mensajeria@gmail.com',
+        pass: 'kjufymkmserzvbpl',
+      },
+    });
+
+    try {
+      let info = await transporter.sendMail({
+        from: '<somosfundacionmas.mensajeria@gmail.com>',
+        to: email,
+        subject: 'Gracias por registrarte a Somos Mas(ONG)',
+        html: `<h2>Hola ${username},</h2>
+        <p>Te has registrado a Somos Mas (ONG). Si no has sido tu, comunicate con nuestro team de soporte</p>`,
+      });
+      return 'Message sent: %s', info.messageId;
+    } catch (err) {
+      return err;
+    }
+  },
+};
