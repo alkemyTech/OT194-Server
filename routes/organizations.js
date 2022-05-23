c1c2584cc8a6453e5dc4c38ab54be0c746839c35
Andrés Siri <andres.siri@hotmail.com>
2022-05-23 12:45:43 -0300

    OT194-17 Create organization public data endpoint - Creación endpoint datos públicos organización

---
+++ routes/organizations.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models/index');
+const Organization = db.Organization;
+
+router.get('/1/public', async function(req, res, next) {
+
+  const organizationData = await Organization.findAll({
+    raw: true,
+    limit: 1
+  });
+
+  const {name, image, phone, address, welcomeText} = organizationData[0];
+
+  res.status(200).json({name, image, phone, address, welcomeText});
+});
+
+module.exports = router;
