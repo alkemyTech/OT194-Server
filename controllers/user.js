51c9909c655cb310d84efd34b2cf473b938b4363
Martin Farres <91481417+Tats01@users.noreply.github.com>
2022-05-29 22:55:50 -0300

    Ot194 44 - Endpoint de Eliminar usuario (#16)
    * Users Delete ( Route & Controller )
    
    * Correccion - Borrado Logico (User)

--- controllers/user.js
+++ controllers/user.js
@@ -1,43 +1,65 @@
 const bcryptjs = require('bcryptjs');
-const user = require('../models/user');
+const db = require('../models');
+const User = db.User;
 
-const register = async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
+const userController = {
+  register: async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
 
-  try {
-    // Email ya esta registrado?
-    const emailExists = await user.findOne({
-      where: {
-        email
+    try {
+      // Email ya esta registrado?
+      const emailExists = await User.findOne({
+        where: {
+          email
+        }
+      });
+
+      if (emailExists) {
+        return res.status(400).json({ msg: 'Email already registered' });
       }
-    });
+      // Encriptamos password
+      const salt = bcryptjs.genSaltSync();
 
-    if (emailExists) {
-      return res.status(400).json({ msg: 'Email already registered' });
+      const hashedPassword = bcryptjs.hashSync(password, salt);
+
+      const userCreated = await User.create({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword
+      });
+
+      return res.status(201).json({
+        ok: true,
+        user: {
+          email: userCreated.email
+        }
+      });
+    } catch (error) {
+      return res.status(500).json({ msg: error });
     }
-    // Encriptamos password
-    const salt = bcryptjs.genSaltSync();
-
-    const hashedPassword = bcryptjs.hashSync(password, salt);
-
-    const userCreated = await user.create({
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword
+  },
+  delete: async (req, res) => {
+    const id = req.params.id;
+    try {
+      await User.update(
+        {
+          firstName: '',
+          lastName: '',
+          email: '',
+          password: '',
+          image: '',
+          deletedAt: Date.now()
+        }, {
+          where: { id }
+        });
+    } catch (error) {
+      return res.status(500).json({ msg: error });
+    }
+    res.status(200).json({
+      msg: `User ${id} deleted Sucessfully`
     });
-
-    return res.status(201).json({
-      ok: true,
-      user: {
-        email: userCreated.email
-      }
-    });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
   }
 };
 
-module.exports = {
-  register
-};
+module.exports = userController;
