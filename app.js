7f1ef5de487b132a492b38884e13c336f7a1ace4
Lali-DMI <lgarribas@dminc.com>
2022-05-30 11:46:04 -0300

    Endpoint Listado de Novedades

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const organizationsRouter = require('./routes/organizations');
 const authRouter = require('./routes/auth');
+const newsRouter = require('./routes/news');
 
 const app = express();
 app.use(cors());
@@ -28,14 +29,15 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/organizations', organizationsRouter);
 app.use('/auth', authRouter);
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
+app.use((_req, _res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
