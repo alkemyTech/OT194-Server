bc3b99e523cd996cb7b5508c5926c14526458b7e
Martin Farres <91481417+Tats01@users.noreply.github.com>
2022-06-01 23:26:11 -0300

    OT194-40  -  Model & Migration ( Entries )  |  Database Folder (#14)
    * Model & Migration - Entries | Database Folder
    
    * Database folder merge conflicts

---
+++ controllers/user.js
@@ -0,0 +1,43 @@
+const bcryptjs = require('bcryptjs');
+const user = require('../database/models/user');
+
+const register = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  try {
+    // Email ya esta registrado?
+    const emailExists = await user.findOne({
+      where: {
+        email
+      }
+    });
+
+    if (emailExists) {
+      return res.status(400).json({ msg: 'Email already registered' });
+    }
+    // Encriptamos password
+    const salt = bcryptjs.genSaltSync();
+
+    const hashedPassword = bcryptjs.hashSync(password, salt);
+
+    const userCreated = await user.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword
+    });
+
+    return res.status(201).json({
+      ok: true,
+      user: {
+        email: userCreated.email
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({ msg: error });
+  }
+};
+
+module.exports = {
+  register
+};
