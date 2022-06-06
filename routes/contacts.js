f1f32a1c4156f8c9044ec65a7dbb5e483a1c5319
urreita9 <francisco.urrea9@gmail.com>
2022-06-06 19:00:35 -0300

    OT194-72 endpoint contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { check } = require('express-validator');
 const { validateFields } = require('../middleware/validateFields');
 const contactsRouter = express.Router();
-const contactsControllers = require('../controllers/organizations');
+const contactsControllers = require('../controllers/contacts');
 
 contactsRouter.post(
 	'/1/public',
@@ -16,7 +16,7 @@ contactsRouter.post(
 		check('name').not().isEmpty().withMessage('Por favor ingrese su nombre'),
 		validateFields,
 	],
-	contactsControllers.public
+	contactsControllers.postContact
 );
 
 module.exports = contactsRouter;
