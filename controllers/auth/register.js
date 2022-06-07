818bcc4b86e468c125210b17cf36f21df059bf3f
DMI <dmi@DMIs-MacBook-Pro.local>
2022-06-06 22:14:46 -0300

    OT194-75 - Enviar email de contacto

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -40,6 +40,24 @@ module.exports = async (req, res) => {
     };
 
     res.status(201).json(createdUser);
+
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: email,
+      from: 'SOMOS MAS !',
+      subject: 'Gracias por registrarte con nosotros',
+      text: 'Estamos muy felices porque nuestra familia se agranda ! Gracias por ser parte de nosotros',
+      html: '<strong>Estamos muy felices porque nuestra familia se agranda !</strong><normal>! Gracias por ser parte de nosotros</normal>'
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        res.send('E-mail enviado');
+      })
+      .catch((error) => {
+        res.send(error);
+      });
   } catch (error) {
     if (req.app.get('env') === 'development') console.log(error);
 
