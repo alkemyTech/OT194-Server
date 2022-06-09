b5e6422c571a3f3abfdfb5235ef70c9c2dcdc234
Lina Gutierrez Arribas <76691894+linamikaela21@users.noreply.github.com>
2022-06-08 22:52:45 -0300

    Endpoint Actualización de la organizacion (#30)
    Co-authored-by: DMI <dmi@DMIs-MacBook-Pro.local>

--- routes/organizations.js
+++ routes/organizations.js
@@ -3,5 +3,6 @@ const organizationsRouter = express.Router();
 const organizationsControllers = require('../controllers/organizations');
 
 organizationsRouter.get('/1/public', organizationsControllers.public);
+organizationsRouter.put('/organization/:id', organizationsControllers.updateOrg);
 
 module.exports = organizationsRouter;
