dc456430dfaef35967311bf9e3d21f0950f1e421
mateojgomez <mateo.gomezcma@gmail.com>
2022-06-09 17:26:13 -0300

    index,  route and middleware

---
+++ middleware/validateNews.js
@@ -0,0 +1,15 @@
+const { body, validationResult } = require('express-validator');
+
+const validateNews = (req, res, next) => {
+  body('name').isLength({ min: 3 }).isString().notEmpty().trim().escape();
+  body('content').isLength({ min: 5 }).isString().notEmpty().trim().escape();
+  body('image').isLength({ min: 10 }).notEmpty().isURL();
+  body('categoryId').isLength({ min: 1 }).isNumeric().notEmpty().trim().escape();
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+module.exports = { validateNews };
--- routes/news.js
+++ routes/news.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const newsRouter = express.Router();
 const newsController = require('../controllers/news');
+const { validateNews } = require('../middleware/validateNews');
 
 newsRouter.get('/', newsController.news);
 
@@ -9,5 +10,7 @@ newsRouter.get('/:id', newsController.newById);
 newsRouter.put('/:id', newsController.updateNewById);
 
 newsRouter.delete('/:id', newsController.deleteNew);
+
+newsRouter.post('/:new', validateNews, newsController.createNew);
 
 module.exports = newsRouter;
