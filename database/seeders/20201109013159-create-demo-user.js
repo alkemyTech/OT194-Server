0d0f68868e92e2a86065480357aa2bc462f68262
Andrés Siri <andres.siri@hotmail.com>
2022-06-08 21:44:56 -0300

    OT194-69 Create activities seeder with info provided by ONG

--- database/seeders/20201109013159-create-demo-user.js
+++ database/seeders/20201109013159-create-demo-user.js
@@ -1,16 +1,33 @@
+const { v4: UUIDV4 } = require('uuid');
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync();
+const hashedPassword = bcrypt.hashSync('123456', salt);
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Users', [{
-      firstName: 'Usuario',
-      lastName: 'Demo',
-      email: 'test@test.com',
-      // Important: Password not encrypted yet!
-      password: '1234',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+    await queryInterface.bulkInsert('Users', [
+      {
+        id: UUIDV4(),
+        firstName: 'Usuario',
+        lastName: 'Demo',
+        email: 'usuariodemo@test.com',
+        password: hashedPassword,
+        roleId: 1,
+        image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        id: UUIDV4(),
+        firstName: 'Admin',
+        lastName: 'Demo',
+        email: 'admindemo@test.com',
+        password: hashedPassword,
+        roleId: 1,
+        image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
