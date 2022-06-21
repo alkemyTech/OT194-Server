710cdb25f13d59db2b6d73f79779158aaa19ec7b
Martin Farres <91481417+Tats01@users.noreply.github.com>
2022-06-21 20:09:13 -0300

    Fix delete user endpoint (#54)

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/users');
 const { adminMiddleware } = require('../middleware/adminCheck');
 const protectRoute = require('../middleware/authentication');
 
-router.put('/:id', userController.delete);
+router.patch('/', protectRoute, userController.delete);
 router.get('/', protectRoute, adminMiddleware, userController.getAllUsers);
 
 module.exports = router;
