f0f539659f69990eda7318fbf929b72e2a7c509c
AnitaLuzardo <77241204+AnitaLuzardo@users.noreply.github.com>
2022-06-21 16:37:23 -0300

    configura Mocha y chai (#53)

---
+++ test/assert.test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('Prueba librereia chai', function () {
+  it('valida el assert', function () {
+    assert.equal(1, 1);
+  });
+});
