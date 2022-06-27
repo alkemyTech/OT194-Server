1fb000167fca0b453d7fc0c2704aef52ca72009d
MijaelFV <fvmijael@gmail.com>
2022-06-27 00:34:35 -0300

    fix char length news & testimonials

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -11,20 +11,24 @@ testimonialsRouter.get('/:id', testimonialsController.getOne);
 testimonialsRouter.get('/', testimonialsController.getAll);
 
 testimonialsRouter.post('/',
-  check('name').not().isEmpty().withMessage('Por favor ingrese un nombre'),
+  check('name')
+    .not().isEmpty().withMessage('Por favor ingrese un nombre')
+    .isLength({ max: 240 }).withMessage('El nombre puede tener un maximo de 240 caracteres'),
   check('content')
     .not().isEmpty().withMessage('Por favor ingrese el contenido')
-    .isLength({ max: 2500 }).withMessage('El contenido puede tener un máximo de 2500 caracteres'),
+    .isLength({ max: 2500 }).withMessage('El contenido puede tener un maximo de 2500 caracteres'),
   validateFields,
   testimonialsController.create);
 
 testimonialsRouter.put('/:id',
   protectRoute,
   adminMiddleware,
-  check('name').not().isEmpty().withMessage('Por favor ingrese un nombre'),
+  check('name')
+    .not().isEmpty().withMessage('Por favor ingrese un nombre')
+    .isLength({ max: 240 }).withMessage('El nombre puede tener un maximo de 240 caracteres'),
   check('content')
     .not().isEmpty().withMessage('Por favor ingrese el contenido')
-    .isLength({ max: 2500 }).withMessage('El contenido puede tener un máximo de 2500 caracteres'),
+    .isLength({ max: 2500 }).withMessage('El contenido puede tener un maximo de 2500 caracteres'),
   validateFields,
   testimonialsController.update);
 
